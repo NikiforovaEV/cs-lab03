29b75670e560200b243e53080366b72d7edc5a02
Rina (NikiforovaEV) <rina@example.com>
2020-04-20 10:49:08 +0300

    code: вынесение отображения гистограммы в функцию show_histogram_text

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-vector <double> input_numbers(size_t count)
+vector<double> input_numbers(size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
@@ -30,7 +30,7 @@ void find_minmax(vector<double> numbers, double& min, double& max)
     }
 }
 
-vector <size_t> make_histogram(vector <double> numbers, size_t bin_count, double min, double max)
+vector<size_t> make_histogram(vector<double> numbers, size_t bin_count, double min, double max)
 {
     vector<size_t> bins(bin_count);
     for (double number : numbers)
@@ -45,6 +45,47 @@ vector <size_t> make_histogram(vector <double> numbers, size_t bin_count, double
         return(bins);
 }
 
+void show_histogram_text(vector<size_t> bins, size_t number_count)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
 int
 main() {
     // Ввод данных
@@ -67,37 +108,6 @@ main() {
     const auto bins = make_histogram(numbers, bin_count, min, max);
 
     // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
-
+    show_histogram_text(bins, number_count);
     return 0;
 }
