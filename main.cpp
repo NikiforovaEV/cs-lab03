8a130e4c72b15bee490811cbad0830a8bbcb59a2
Rina (NikiforovaEV) <rina@example.com>
2020-04-20 10:15:21 +0300

    code: вынесение поиска минимума и максимума в отдельную функцию find_minmax

--- main.cpp
+++ main.cpp
@@ -13,6 +13,23 @@ vector<double> input_numbers(size_t count)
     return result;
 }
 
+void find_minmax(vector<double> numbers, double& min, double& max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+}
+
 int
 main() {
     // ¬вод данных
@@ -28,16 +45,9 @@ main() {
     cin >> bin_count;
 
     // ќбработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
+    double min;
+    double max;
+    find_minmax(numbers, min, max);
 
     vector<size_t> bins(bin_count);
     for (double number : numbers) {
