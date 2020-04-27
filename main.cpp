ecf98dd6cf356ba57c0facffc0f2768c06bb0416
Rina (NikiforovaEV) <rina@example.com>
2020-04-27 10:25:08 +0300

    code: перенесение определения функции find_minmax в histogram.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -11,23 +12,6 @@ vector<double> input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-
-void find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
-    }
 }
 
 vector<size_t> make_histogram(vector<double> numbers, size_t bin_count, double min, double max)
