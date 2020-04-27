ca34e78258fa7578952d257e0b0fe5eea501b45d
Rina (NikiforovaEV) <rina@example.com>
2020-04-27 14:51:20 +0300

    code: выделение в отдельный модуль всех функций для работы с SVG

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
+#include "svg_histogram.h"
 
 using namespace std;
 
@@ -69,55 +70,6 @@ void show_histogram_text(vector<size_t> bins, size_t number_count)
         }
         cout << '\n';
     }
-}
-
-void svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "green")
-{
-    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
-}
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
-}
-
-void show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    //svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    //svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
-    double top = 0;
-    //string stroke = "black";
-    //string fill = "green";
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "ûblack", "green");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
 }
 
 int
