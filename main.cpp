5b762762806fa5d692450cb680e5f13be9bcc939
Rina (NikiforovaEV) <rina@example.com>
2020-06-05 12:47:03 +0300

    code: добавлено сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -42,11 +42,35 @@ Input read_input(istream& in, bool prompt)
     return data;
 }
 
-Input
-download(const string& address) {
-    stringstream buffer;
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
+{
+    const size_t data_size = item_size * item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return 0;
+}
 
-    // TODO: заполнить буфер.
+Input download(const string& address)
+{
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL* curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+            res = curl_easy_perform(curl);
+            res = CURLE_FAILED_INIT;
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
+            curl_easy_cleanup(curl);
+        }
 
     return read_input(buffer, false);
 }
@@ -113,33 +137,18 @@ void show_histogram_text(vector<size_t> bins, size_t number_count)
 
 int main(int argc, char* argv[])
 {
-    curl_global_init(CURL_GLOBAL_ALL);
+    Input input;
     if (argc>1)
     {
-        CURL* curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            res = CURLE_FAILED_INIT;
-            if (res != CURLE_OK)
-            {
-                cout << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return(0);
+        input = download(argv[1]);
     }
     else
     {
-        // Ввод данных
-        const auto input = read_input(cin, true);
+        input = read_input(cin, true);
+    }
         // Обработка данных
         const auto bins = make_histogram(input);
         // Вывод данных
         show_histogram_svg(bins);
         return 0;
-    }
 }
